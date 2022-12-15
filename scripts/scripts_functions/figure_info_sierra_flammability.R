c1c583c33078ca80555d11faf39f5b1b68bebc78
bovingigit config --global user.name Jane <indraboving@gmail.com>
2022-12-15 10:28:03 -0800

    colors, var decomps, etc.

--- scripts/scripts_functions/figure_info_sierra_flammability.R
+++ scripts/scripts_functions/figure_info_sierra_flammability.R
@@ -1,6 +1,7 @@
 library(calecopal)
 #To install above package: devtools::install_github("an-bui/calecopal")
 library(MetBrewer)
+library(tidyverse)
 #make ggplot prettier
 
 MetBrewer::colorblind_palettes
@@ -15,16 +16,55 @@ ggplot <- function(...) { ggplot2::ggplot(...) +
 color_noceco <-  scale_color_manual(values = c('black', '#BD282F', '#FDD358', '#87B37A', '#5D5D81')) 
 color_many2 <- scale_color_manual(values = c('black', '#BD282F', '#EE8739', '#FDD358', '#87B37A', '#5D5D81'))
 
-#Met Brewer Colors
+#Calecopal Colors
 
-color_many <- scale_color_manual(values = met.brewer("Tam", 6, direction = -1, type = "discrete")) 
+sierra1 = c("#BD973D", "#5F5C29", "#3B7D6E", "#5792CC", "#4D5B75", "#262E43")
 
-color_two <- scale_color_manual(values = met.brewer("Tam", 4 ,direction = -1)) 
+#To get more colors, if needed:
+nb.cols.10 <- 1
+pal.10 <- cal_palette("sierra1")
+mycolors.10 <- colorRampPalette(pal.10)(nb.cols.10)
 
-color_fill <- scale_fill_manual(values = met.brewer("Tam"))
+color_many <- scale_color_manual(values = cal_palette("sierra1", 6, #direction = -1, 
+                                                      type = "discrete")) 
 
-color_grad <- scale_color_gradientn(colors=met.brewer("Tam", direction = -1))
+color_two <- scale_color_manual(values = cal_palette("sierra1", 4 
+                                                     #direction = -1)
+                                                     ))
 
-color_all <- scale_color_manual(values = met.brewer("Tam", direction = -1, type = "discrete")) 
+color_fill <- scale_fill_manual(values = cal_palette("sierra1"))
+
+color_grad <- scale_color_gradientn(colors=cal_palette("sierra1", 
+                                                       #direction = -1)
+                                                       ))
+
+color_all <- scale_color_manual(values = cal_palette("sierra1", 
+                                                     #direction = -1, 
+                                                     type = "discrete")) 
 
 axis_size <- theme(axis_text = element_text(size = 24))
+
+
+#variance decomp colors: 
+  vcol <-  c(age_new = "purple", 
+             spp = "pink", 
+             year = "blue", 
+             year_month = "blue",
+             water_potential = "cyan", 
+             plant = "green", 
+             F.Group = "gold", 
+             Residuals = "grey")
+
+#Met Brewer Colors
+
+# color_many <- scale_color_manual(values = met.brewer("sierra1", 6, direction = -1, type = "discrete")) 
+# 
+# color_two <- scale_color_manual(values = met.brewer("sierra1", 4 ,direction = -1)) 
+# 
+# color_fill <- scale_fill_manual(values = met.brewer("sierra1"))
+# 
+# color_grad <- scale_color_gradientn(colors=met.brewer("sierra1", direction = -1))
+# 
+# color_all <- scale_color_manual(values = met.brewer("sierra1", direction = -1, type = "discrete")) 
+# 
+# axis_size <- theme(axis_text = element_text(size = 24))
