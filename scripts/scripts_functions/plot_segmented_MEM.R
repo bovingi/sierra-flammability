4ed86157e0bc480d7c163d91da468949b92392e1
celebrezze <celebrezze@ucsb.edu>
2023-03-09 09:11:53 -0800

    Continuing with GitHub reorganization; deleting extra datasets, moving scripts to 'extra analyses', relabelling figures, removing extra code in scripts, debugging, etc. - commit 1/3 (from 3/8/23)

--- scripts/scripts_functions/plot_segmented_MEM.R
+++ scripts/scripts_functions/plot_segmented_MEM.R
@@ -1,14 +1,14 @@
 
-rsegplot.lfm <- function(model, dataframe, species, x.variable = 'lfm', y.variable, max.x = max.lfm, x.lab = 'Live Fuel Moisture (%)', y.lab = NULL, tlp = NULL, tlp.upper = NULL, tlp.lower = NULL, color = 'color') {
+rsegplot.lfm <- function(model, dataframe, x.variable = 'lfm', y.variable, max.x,
+                         x.lab = 'Live Fuel Moisture (%)', y.lab = NULL, tlp = NULL, tlp.upper = NULL, tlp.lower = NULL) {
   
   p <- summary.segmented(model)$psi[2]
   m <- slope(model, .coef = fixef(model))[[1]][[1]]
   b <- intercept(model, .coef = fixef(model))[[1]][[1]]
   m2 <- slope(model, .coef = fixef(model))[[1]][[2]]
   b2 <- intercept(model, .coef = fixef(model))[[1]][[2]]
-  yvar <- dataframe[,y.variable]
-  xvar <- dataframe[,x.variable]
-  colr <- dataframe[,color]
+  yvar <- dataframe[[y.variable]]
+  xvar <- dataframe[[x.variable]]
   confint <- confint.segmented(model, x.variable, .coef=fixef(model))
   lower <- confint[2]
   upper <- confint[3]
@@ -17,23 +17,11 @@ rsegplot.lfm <- function(model, dataframe, species, x.variable = 'lfm', y.variab
   
   wacky <- plot(xvar, yvar, xlim = c(0, max.x), ylim = c(ymin-0.05*ymin, ymax+0.05*ymax), 
                 xlab = x.lab, ylab = y.lab,
-                pch = 16, col = alpha(colr, 0.8),
+                pch = 16, col = alpha('black', 0.8),
                 cex.lab = 1.3)
   polygon(x = c(lower, upper, upper, lower), y = c(ymax + 0.2*ymax, ymax +0.2*ymax, ymin - 30, ymin - 30), col = alpha('gray', 0.3), lty = 2)
   abline(v = tlp, col = alpha('#00C368', 0.7), cex = 3)
   polygon(x = c(tlp.lower, tlp.upper, tlp.upper, tlp.lower), y = c(ymax +0.2*ymax, ymax +0.2*ymax, ymin -30, ymin -30), col = alpha('#00C368', 0.3), border = alpha('#00C368', 0.6))
-  legend('topleft', legend=if(species == 'ADFA'){
-    c("September 2020", "January 2018", "December 2016")
-  }
-  else if(species == 'CEME'){
-    c("September 2020", "January 2020", "December 2019")
-  },
-  col=if(species == "ADFA") {
-    c("#c1121f","#FF7262","#A4A7DE")
-  }
-  else if(species == "CEME") {
-    c("#c1121f","#64A400","#669BBC")
-  }, pch = 16, cex=1)
   for(z in 1:length(xvar)){
     if(z < p){
       segments(x0 = 0, x1 = p, y0 = b, y1 = m*((b2 - b)/(m - m2))+b, lwd = 3, col = "#9D0208")
@@ -43,66 +31,16 @@ rsegplot.lfm <- function(model, dataframe, species, x.variable = 'lfm', y.variab
 }
 
 
-
-rsegplot.mpa <- function(model, dataframe, species, x.variable = 'mpa', y.variable, max.x = max.mpa, x.lab = 'Water Potential (MPa)', y.lab = NULL, tlp = NULL, tlp.upper = NULL, tlp.lower = NULL, color = 'color') 
-{
-  p <- summary.segmented(model)$psi[2]
-  m <- slope(model, .coef = fixef(model))[[1]][[1]]
-  b <- intercept(model, .coef = fixef(model))[[1]][[1]]
-  m2 <- slope(model, .coef = fixef(model))[[1]][[2]]
-  b2 <- intercept(model, .coef = fixef(model))[[1]][[2]]
-  yvar <- dataframe[,y.variable]
-  xvar <- dataframe[,x.variable]
-  colr <- dataframe[,color]
-  confint <- confint.segmented(model, x.variable, .coef=fixef(model))
-  lower <- confint[2]
-  upper <- confint[3]
-  ymax <- max(yvar)
-  ymin <- min(yvar)
-  
-  wacky <- plot(xvar, yvar, xlim = c(-10, 0), ylim = c(ymin-1, ymax+1), 
-                xlab = x.lab, ylab = y.lab,
-                pch = 16, col = colr, cex.lab = 1.1)
-  polygon(x = c(lower, upper, upper, lower), y = c(ymax +1.5, ymax +1.5, ymin -1.5, ymin -1.5), col = alpha('gray', 0.3), lty = 2)
-  abline(v = tlp, col = alpha('#00C368', 0.8), cex = 3)
-  polygon(x = c(tlp.lower, tlp.upper, tlp.upper, tlp.lower), y = c(ymax +1.5, ymax +1.5, ymin -1.5, ymin -1.5), col = alpha('#00C368', 0.3), border = alpha('#00C368', 0.7))
-  abline(a = b, b = m, lwd = 3, col = "#9D0208")
-  legend(-2, ymax+0.05*ymax, legend=if(species == 'ADFA'){
-    c("September 2020", "January 2018", "December 2016")
-  }
-  else if(species == 'CEME'){
-    c("September 2020", "January 2020", "December 2019")
-  },
-  col=if(species == "ADFA") {
-    c("#c1121f","#FF7262","#A4A7DE")
-  }
-  else if(species == "CEME") {
-    c("#c1121f","#64A400","#669BBC")
-  }, pch = 16, cex=0.8)
-  for(j in 1:length(xvar)){
-    if(j < p){
-      segments(x0 = -10, x1 = p, y0 = (-10*m) + b , y1 = m2*((b - b2)/(m2 - m))+b2, lwd = 3, col = "#9D0208")
-    }
-    else{
-      segments(x0 = p, x1 = max.x, y0 = m2*((b - b2)/(m2 - m))+b2, y1 = b2, lwd = 3, col = "#9D0208")
-    }
-  }
-  return(wacky)
-}
-
-
-#For splitting by sampling date: no legend!
-
-rsegplot.lfm.nolegend <- function(model, dataframe, species, x.variable = 'lfm', y.variable, max.x = max.lfm, x.lab = 'Live Fuel Moisture (%)', y.lab = NULL, tlp = NULL, tlp.upper = NULL, tlp.lower = NULL, color = 'color') {
+rsegplot.mpa <- function(model, dataframe, x.variable = 'mpa', y.variable, 
+                         max.x, x.lab = 'Water Potential (MPa)', y.lab = NULL, tlp = NULL, tlp.upper = NULL, tlp.lower = NULL) {
   
   p <- summary.segmented(model)$psi[2]
   m <- slope(model, .coef = fixef(model))[[1]][[1]]
   b <- intercept(model, .coef = fixef(model))[[1]][[1]]
   m2 <- slope(model, .coef = fixef(model))[[1]][[2]]
   b2 <- intercept(model, .coef = fixef(model))[[1]][[2]]
-  yvar <- dataframe[,y.variable]
-  xvar <- dataframe[,x.variable]
-  colr <- dataframe[,color]
+  yvar <- dataframe[[y.variable]]
+  xvar <- dataframe[[x.variable]]
   confint <- confint.segmented(model, x.variable, .coef=fixef(model))
   lower <- confint[2]
   upper <- confint[3]
@@ -111,7 +49,7 @@ rsegplot.lfm.nolegend <- function(model, dataframe, species, x.variable = 'lfm',
   
   wacky <- plot(xvar, yvar, xlim = c(0, max.x), ylim = c(ymin-0.05*ymin, ymax+0.05*ymax), 
                 xlab = x.lab, ylab = y.lab,
-                pch = 16, col = alpha(colr, 0.8),
+                pch = 16, col = alpha('black', 0.8),
                 cex.lab = 1.3)
   polygon(x = c(lower, upper, upper, lower), y = c(ymax + 0.2*ymax, ymax +0.2*ymax, ymin - 30, ymin - 30), col = alpha('gray', 0.3), lty = 2)
   abline(v = tlp, col = alpha('#00C368', 0.7), cex = 3)
